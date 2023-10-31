c281d3807b99494e7a71495c4293d343ab009aa5
AlbertoBarba1997 <juanalbertobarba1997@gmail.com>
2023-10-31 14:27:36 +0100

    3. Cabecera CSS mejorado. Añadido nombre de usuario de Session Storage

--- src/app/components/jugador/cabecera/cabecera.component.ts
+++ src/app/components/jugador/cabecera/cabecera.component.ts
@@ -1,10 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-cabecera',
   templateUrl: './cabecera.component.html',
   styleUrls: ['./cabecera.component.css']
 })
-export class CabeceraComponent {
+export class CabeceraComponent implements OnInit{
+  usuarioLogueado: string;
 
+  constructor(private router: Router) {
+    this.usuarioLogueado = "Usuario";
+  }
+
+
+  ngOnInit(): void {
+    try{
+      const userString= sessionStorage.getItem("user");
+
+      if(userString!= null){
+        const user = JSON.parse(userString!);
+        let nombre= user.name;
+        this.usuarioLogueado = nombre;
+        //Cargar imagen redonda :)
+
+      }else{
+        this.usuarioLogueado = "Usuario no logueado";
+      }
+
+
+    }catch(err){
+      this.usuarioLogueado = "Nombre de Usuario";
+    }
+
+  }
+
+  navigateToHome() {
+    this.router.navigate(['/jugadores/home']); // Navegar a la página de inicio
+  }
 }
